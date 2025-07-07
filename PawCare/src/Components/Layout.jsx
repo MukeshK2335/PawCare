a717ec354763cd871ce2cb58c51e43a5a4d36261
pradheesh <pradheeshj4@gmail.com>
2025-07-07 20:32:37 +0530

    added layout

---
+++ PawCare/src/Components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Layout = ()=>{
+    return (
+        <div className="LayoutContainer">
+            <h1 className="heading">PawCare</h1>
+            <nav>
+                <a href="">Dashboard</a>
+                <a href="">Reports</a>
+            </nav>
+        </div>
+    )
+}
+export default Layout
